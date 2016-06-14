fb1c2e5c793dd2ce698c311d78d080793727ae49
bokoboko765 <bokoboko765@yahoo.co.jp>
2016-06-14 10:05:45 +0900

    Update keymap.c

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -37,13 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,         KC_1,         KC_2,        KC_3,      KC_4,   KC_5,   KC_F13,
-        KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,   KC_COMM,
+        KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,   KC_QUOT,
         LT(KSL,KC_TRNS),KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,   KC_F13,
         KC_LCTRL,       KC_LGUI,      LT(NUM,KC_TRNS),TG(NUM),KC_LALT,
                                                                LT(NUM,KC_TRNS),KC_TRNS,
                                                                               KC_PSCR,
-                                                               KC_SPC,LT(NUM,KC_TRNS),KC_F13,
+                                                               KC_SPC,LT(NUM,KC_TRNS),LT(FN,KC_TRNS),
         // right hand
              KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
              KC_MINS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PSCR,     KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,           KC_F11,
              KC_TRNS,     KC_HOME,KC_DELT,KC_BSPC,KC_END, KC_TRNS,          KC_F12,
                           KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_TRNS,          KC_ENT,
-             KC_TRNS,     KC_GRV, KC_COMM,KC_EQL, KC_MINS,KC_PGUP,          KC_TRNS,
+             KC_TRNS,     KC_GRV, KC_QUOT,KC_EQL, KC_MINS,KC_PGUP,          KC_TRNS,
                                   KC_LBRC,KC_RBRC,KC_HOME,KC_PGDN,          KC_END,
              KC_TRNS,        KC_TRNS,
              KC_TRNS,
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_LCTL,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_LALT,
                                                             KC_TRNS, KC_TRNS,
                                                                      KC_TRNS,
-                                                     KC_SPC, KC_BSPC,KC_TRNS,
+                                                     KC_SPC, KC_TRNS,KC_TRNS,
        // right hand
        KC_NLCK, KC_F7,   KC_F8,   KC_SLSH, KC_ASTR,  KC_TRNS, KC_BSPC,
        KC_TRNS, KC_MINS, KC_7,    KC_8,    KC_9,     KC_SLSH, KC_MINS,
