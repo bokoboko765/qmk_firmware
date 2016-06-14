e7a1de4bf85f68a154ebb0dc092827aa29ed7912
bokoboko765 <bokoboko765@yahoo.co.jp>
2016-06-14 21:03:55 +0900

    Update keymap.c

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
              KC_MINS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_SLSH,
+                          KC_H,   KC_J,   KC_K,   KC_L,   LT(FN,KC_SCLN),          KC_SLSH,
              KC_EQL,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,            LT(FN,KC_TRNS),
                                   KC_F13, KC_RCTL,KC_LEFT,KC_DOWN,          KC_RGHT,
              KC_INS,         KC_DELT,
