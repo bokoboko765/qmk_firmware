6b8258a3f87dc2c95bcff7138afded9dfa175670
bokoboko765 <bokoboko765@yahoo.co.jp>
2016-07-26 09:31:41 +0900

    Update keymap.c

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -16,11 +16,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | ESC    |   1  |   2  |   3  |   4  |   5  | F13  |           | `~   |   6  |   7  |   8  |   9  |   0  | Bkspc  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | copy |           | _    |   Y  |   U  |   I  |   O  |   P  |   \|   |
- * |--------+------+------+------+------+------|      |           | -    |------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | copy |           | +    |   Y  |   U  |   I  |   O  |   P  |   \|   |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | =/~L1  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  | ;/L3 |   /?   |
- * |--------+------+------+------+------+------|paste |           | +    |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl| X/L2 |   C  |   V  |   B  |      |           | =    |   N  |   M  |   ,  |   .  |  Up  |  '/L3  |
+ * |--------+------+------+------+------+------|paste |           | *    |------+------+------+------+------+--------|
+ * | LShift |Z/Ctrl| X/L2 |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  Up  |  '/L3  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCtrl | LGui |paste | copy | LAlt |                                       | kana |RCtrl | Left | Down | Right|
  *   `----------------------------------'                                       `----------------------------------'
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                KC_SPC,LT(NUM,KC_TRNS),LT(FN,KC_TRNS),
         // right hand
              KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
-             KC_MINS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+             KC_PLUS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(FN,KC_SCLN),          KC_SLSH,
-             KC_EQL,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,            LT(FN,KC_QUOT),
+             KC_ASTR,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,            LT(FN,KC_QUOT),
                                   LALT(KC_GRV), KC_RCTL,KC_LEFT,KC_DOWN,          KC_RGHT,
              KC_INS,         KC_DELT,
              KC_TRNS,
